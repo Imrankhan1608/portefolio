95d8ccaa16e353119d7b5e1d8cd7a90b208efcef
Imrankhan1608 <zainirran932@gmail.com>
2025-05-12 22:00:15 +0300

    portefolio

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended:false}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-app.post('/send', (req,res) => {
-    const { name ,number ,email, message } = req.body;
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'zainirran932@gmail.com',
-            pass: 'fatanabi',
-        },
-    });
-
-    const mailOptions = {
-        from: email,
-        to: 'destinataire@gmail.com',
-        subject:'Message de ${name}',
-        text: message,
-    };
-    transporter.sendMail(mailOptions,(error, info) => {
-        if (error) {
-            console.log(error);
-            res.send('Erreur ,ressayez');
-        } else {
-            console.log('Email envoyé : ' + info.response);
-            res.send('Email bien envoyé !');
-        }
-    });
+// Pour afficher un formulaire simple (facultatif)
+app.get('/', (req, res) => {
+  res.send(`seveur actif`);
 });
 
+// Envoi d'email avec Nodemailer
+app.post('/send', (req, res) => {
+  const { name, email, message } = req.body;
 
-app.get('/', (req,res) => {
-    res.send('serveur actif');
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'zainirran932@gmail.com',
+      pass: 'fatanabi',
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'zainirran932@gmail.com',
+    subject: `Message de ${name}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Erreur lors de l\'envoi');
+    } else {
+      console.log('Email envoyé : ' + info.response);
+      res.send('Email bien envoyé !');
+    }
+  });
 });
 
-
-
+// Lancement du serveur
 app.listen(3000, () => {
-    console.log('serveur actif sur http://localhost:3000');
+  console.log('Serveur démarré sur http://localhost:3000');
 });
